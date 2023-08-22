ee96d2aba911d2e110cf61680ff0c774feceb4b6
AzureQian <azureqianhz@gmail.com>
2023-08-23 00:30:28 +0800

    Finish [Basics-Mathematical Surfaces]

--- Basic/Assets/Scripts/Game Objects and Scripts/Clock.cs
+++ Basic/Assets/Scripts/Game Objects and Scripts/Clock.cs
@@ -1,17 +1,21 @@
 using System;
 using UnityEngine;
 
-public class Clock : MonoBehaviour
+namespace game_objects_and_scripts
 {
-    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+    public class Clock : MonoBehaviour
+    {
+        const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
     
-    [SerializeField]
-    Transform hoursPivot, minutesPivot, secondsPivot;
+        [SerializeField]
+        Transform hoursPivot, minutesPivot, secondsPivot;
 
-    void Update () {
-        TimeSpan time = DateTime.Now.TimeOfDay;
-        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
-        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
-        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+        void Update () {
+            TimeSpan time = DateTime.Now.TimeOfDay;
+            hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float)time.TotalHours);
+            minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float)time.TotalMinutes);
+            secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float)time.TotalSeconds);
+        }
     }
 }
+
